8c32f2e37d4a41eaaa8d328c7c97b7f2e7109f41
Alexis Newby <alexis.newby@gmail.com>
2022-04-19 12:06:14 -0600

    Add needed components' skeletons and set routes

---
+++ src/Components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <h2>Nav Bar</h2>
+  )
+
+}
+
+export default NavBar
---
+++ src/Components/SingleArticle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SingleArticle = () => {
+
+}
+
+export default SingleArticle
