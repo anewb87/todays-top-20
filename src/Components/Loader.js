d075e77313e6971d93e0a3b1068370883628c21b
Alexis Newby <alexis.newby@gmail.com>
2022-04-19 14:44:54 -0600

    Add loader

---
+++ src/Components/Loader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../Styling/Loader.scss';
+
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <div className='loader-container'>
+        <div className='loader__bar'></div>
+        <div className='loader__bar'></div>
+        <div className='loader__bar'></div>
+        <div className='loader__bar'></div>
+        <div className='loader__bar'></div>
+        <div className='loader__ball'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
+
+//https://codepen.io/ispal/pen/mVaaJe
