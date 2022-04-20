e9c7d7dda70b5ffbb5e63582740b7e4016028ed7
Alexis Newby <alexis.newby@gmail.com>
2022-04-19 20:13:10 -0600

    Individual article display functionality

--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Styling/ArticleCard.scss'
 
 const ArticleCard = ({ id, title, photo }) => {
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'black'
+  };
 
   return (
     <div className='article-card'>
-      <h2>{title}</h2>
-      <img className='thumbnail-pic' src={photo} alt={`Photo for article: ${title}`}/>
+      <Link style={linkStyle} to={`article/${id}`}>
+        <h2>{title}</h2>
+        <img className='thumbnail-pic' src={photo} alt={`Photo for article: ${title}`}/>
+      </Link>
     </div>
   )
 }
